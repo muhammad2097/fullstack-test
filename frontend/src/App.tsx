1e779e75dadbb8681ee9ad7817ba8674a4ca5987
hafiz muhammad <hafiz.muhammad@voxolo.gy>
2025-11-29 10:29:30 +0500

    filtered functionality added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,30 +1,64 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useMemo, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
-function App() {  
+function App() {
   const [users, setUsers] = useState([]);
+  const [filter, setFilter] = useState("");
 
   useEffect(() => {
-
     fetch("http://localhost:3000/api/users")
-    .then(res => res.json())
-    .then(res => {
-      setUsers(res);
-      console.log(res);
-    })
+      .then((res) => res.json())
+      .then((res) => {
+        setUsers(res);
+        console.log(res);
+      });
+  }, []);
 
-  }, [])
+  // change func
+  const changeFn = (e) => {
+    setFilter(e.target.value);
+  };
 
+  // filtered by name
+  const filtered = useMemo(() => {
+    return users.filter((u) => u.name.toLowerCase().includes(filter));
+  }, [users, filter]);
+
+  console.log(filtered);
+
+  // implement debounce
+  // function debounce(callback, delay) {
+  //   let timer;
+
+  //   return (...args) {
+  //     timer = setTimeout(() => callback(...args), delay);
+  //     clearTimeout(timer);
+  //   }
+  // }
+  
   return (
     <>
-    {users.map( => {
-      <p>{u}</p>
-    })}
-      <p>Initial page</p>
+      Search by name:
+      <input
+        style={{ padding: "10px", margin: "10px" }}
+        type="text"
+        onChange={changeFn}
+      />
+      <ul>
+        {filtered.map((user) => (
+          <div style={{ textAlign: "left" }}>
+            <input type="checkbox" value={user.favourite} />
+            <li key={user.id} style={{ textAlign: "left" }}>
+              {user.name} - {user.email} - {user.company.name}
+            </li>
+          </div>
+        ))}
+      </ul>
+      {/* <p>Initial page</p> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
